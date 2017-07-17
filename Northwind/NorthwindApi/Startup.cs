4d39364ad3474955479c7835fc351b4b11c8b3b5
Jason Taylor <jasongt@outlook.com>
2017-07-17 21:44:59 +1000

    Scaffolding finished.

--- Northwind/NorthwindApi/Startup.cs
+++ Northwind/NorthwindApi/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using NorthwindTraders.Persistence;
 
 namespace NorthwindTraders.NorthwindApi
 {
@@ -26,15 +27,20 @@ namespace NorthwindTraders.NorthwindApi
         {
             // Add framework services.
             services.AddMvc();
+
+            services.AddDbContext<NorthwindContext>(options => 
+                    options.UseSqlServer(Configuration.GetConnectionString("NorthwindContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, NorthwindContext context)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
             app.UseMvc();
+
+            NorthwindInitializer.Initialize(context);
         }
     }
 }
---
+++ Northwind/Persistence/Migrations/20170717114410_InitialCreate.Designer.cs
@@ -0,0 +1,468 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using NorthwindTraders.Persistence;
+
+namespace NorthwindTraders.Persistence.Migrations
+{
+    [DbContext(typeof(NorthwindContext))]
+    [Migration("20170717114410_InitialCreate")]
+    partial class InitialCreate
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("NorthwindTraders.Domain.Category", b =>
+                {
+                    b.Property<int>("CategoryId")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnName("CategoryID");
+
+                    b.Property<string>("CategoryName")
+                        .IsRequired()
+                        .HasMaxLength(15);
+
+                    b.Property<string>("Description")
+                        .HasColumnType("ntext");
+
+                    b.Property<byte[]>("Picture")
+                        .HasColumnType("image");
+
+                    b.HasKey("CategoryId");
+
+                    b.ToTable("Categories");
+                });
+
+            modelBuilder.Entity("NorthwindTraders.Domain.Customer", b =>
+                {
+                    b.Property<string>("CustomerId")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnName("CustomerID")
+                        .HasColumnType("nchar(5)");
+
+                    b.Property<string>("Address")
+                        .HasMaxLength(60);
+
+                    b.Property<string>("City")
+                        .HasMaxLength(15);
+
+                    b.Property<string>("CompanyName")
+                        .IsRequired()
+                        .HasMaxLength(40);
+
+                    b.Property<string>("ContactName")
+                        .HasMaxLength(30);
+
+                    b.Property<string>("ContactTitle")
+                        .HasMaxLength(30);
+
+                    b.Property<string>("Country")
+                        .HasMaxLength(15);
+
+                    b.Property<string>("Fax")
+                        .HasMaxLength(24);
+
+                    b.Property<string>("Phone")
+                        .HasMaxLength(24);
+
+                    b.Property<string>("PostalCode")
+                        .HasMaxLength(10);
+
+                    b.Property<string>("Region")
+                        .HasMaxLength(15);
+
+                    b.HasKey("CustomerId");
+
+                    b.ToTable("Customers");
+                });
+
+            modelBuilder.Entity("NorthwindTraders.Domain.Employee", b =>
+                {
+                    b.Property<int>("EmployeeId")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnName("EmployeeID");
+
+                    b.Property<string>("Address")
+                        .HasMaxLength(60);
+
+                    b.Property<DateTime?>("BirthDate")
+                        .HasColumnType("datetime");
+
+                    b.Property<string>("City")
+                        .HasMaxLength(15);
+
+                    b.Property<string>("Country")
+                        .HasMaxLength(15);
+
+                    b.Property<string>("Extension")
+                        .HasMaxLength(4);
+
+                    b.Property<string>("FirstName")
+                        .IsRequired()
+                        .HasMaxLength(10);
+
+                    b.Property<DateTime?>("HireDate")
+                        .HasColumnType("datetime");
+
+                    b.Property<string>("HomePhone")
+                        .HasMaxLength(24);
+
+                    b.Property<string>("LastName")
+                        .IsRequired()
+                        .HasMaxLength(20);
+
+                    b.Property<string>("Notes")
+                        .HasColumnType("ntext");
+
+                    b.Property<byte[]>("Photo")
+                        .HasColumnType("image");
+
+                    b.Property<string>("PhotoPath")
+                        .HasMaxLength(255);
+
+                    b.Property<string>("PostalCode")
+                        .HasMaxLength(10);
+
+                    b.Property<string>("Region")
+                        .HasMaxLength(15);
+
+                    b.Property<int?>("ReportsTo");
+
+                    b.Property<string>("Title")
+                        .HasMaxLength(30);
+
+                    b.Property<string>("TitleOfCourtesy")
+                        .HasMaxLength(25);
+
+                    b.HasKey("EmployeeId");
+
+                    b.HasIndex("ReportsTo");
+
+                    b.ToTable("Employees");
+                });
+
+            modelBuilder.Entity("NorthwindTraders.Domain.EmployeeTerritory", b =>
+                {
+                    b.Property<int>("EmployeeId")
+                        .HasColumnName("EmployeeID");
+
+                    b.Property<string>("TerritoryId")
+                        .HasColumnName("TerritoryID")
+                        .HasMaxLength(20);
+
+                    b.HasKey("EmployeeId", "TerritoryId")
+                        .HasName("PK_EmployeeTerritories");
+
+                    b.HasIndex("TerritoryId");
+
+                    b.ToTable("EmployeeTerritories");
+                });
+
+            modelBuilder.Entity("NorthwindTraders.Domain.Order", b =>
+                {
+                    b.Property<int>("OrderId")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnName("OrderID");
+
+                    b.Property<string>("CustomerId")
+                        .HasColumnName("CustomerID")
+                        .HasColumnType("nchar(5)");
+
+                    b.Property<int?>("EmployeeId")
+                        .HasColumnName("EmployeeID");
+
+                    b.Property<decimal?>("Freight")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("money")
+                        .HasDefaultValueSql("0");
+
+                    b.Property<DateTime?>("OrderDate")
+                        .HasColumnType("datetime");
+
+                    b.Property<DateTime?>("RequiredDate")
+                        .HasColumnType("datetime");
+
+                    b.Property<string>("ShipAddress")
+                        .HasMaxLength(60);
+
+                    b.Property<string>("ShipCity")
+                        .HasMaxLength(15);
+
+                    b.Property<string>("ShipCountry")
+                        .HasMaxLength(15);
+
+                    b.Property<string>("ShipName")
+                        .HasMaxLength(40);
+
+                    b.Property<string>("ShipPostalCode")
+                        .HasMaxLength(10);
+
+                    b.Property<string>("ShipRegion")
+                        .HasMaxLength(15);
+
+                    b.Property<int?>("ShipVia");
+
+                    b.Property<DateTime?>("ShippedDate")
+                        .HasColumnType("datetime");
+
+                    b.HasKey("OrderId");
+
+                    b.HasIndex("CustomerId");
+
+                    b.HasIndex("EmployeeId");
+
+                    b.HasIndex("ShipVia");
+
+                    b.ToTable("Orders");
+                });
+
+            modelBuilder.Entity("NorthwindTraders.Domain.OrderDetail", b =>
+                {
+                    b.Property<int>("OrderId")
+                        .HasColumnName("OrderID");
+
+                    b.Property<int>("ProductId")
+                        .HasColumnName("ProductID");
+
+                    b.Property<float>("Discount")
+                        .ValueGeneratedOnAdd()
+                        .HasDefaultValueSql("0");
+
+                    b.Property<short>("Quantity")
+                        .ValueGeneratedOnAdd()
+                        .HasDefaultValueSql("1");
+
+                    b.Property<decimal>("UnitPrice")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("money")
+                        .HasDefaultValueSql("0");
+
+                    b.HasKey("OrderId", "ProductId")
+                        .HasName("PK_Order_Details");
+
+                    b.HasIndex("ProductId");
+
+                    b.ToTable("Order Details");
+                });
+
+            modelBuilder.Entity("NorthwindTraders.Domain.Product", b =>
+                {
+                    b.Property<int>("ProductId")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnName("ProductID");
+
+                    b.Property<int?>("CategoryId")
+                        .HasColumnName("CategoryID");
+
+                    b.Property<bool>("Discontinued")
+                        .ValueGeneratedOnAdd()
+                        .HasDefaultValueSql("0");
+
+                    b.Property<string>("ProductName")
+                        .IsRequired()
+                        .HasMaxLength(40);
+
+                    b.Property<string>("QuantityPerUnit")
+                        .HasMaxLength(20);
+
+                    b.Property<short?>("ReorderLevel")
+                        .ValueGeneratedOnAdd()
+                        .HasDefaultValueSql("0");
+
+                    b.Property<int?>("SupplierId")
+                        .HasColumnName("SupplierID");
+
+                    b.Property<decimal?>("UnitPrice")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("money")
+                        .HasDefaultValueSql("0");
+
+                    b.Property<short?>("UnitsInStock")
+                        .ValueGeneratedOnAdd()
+                        .HasDefaultValueSql("0");
+
+                    b.Property<short?>("UnitsOnOrder")
+                        .ValueGeneratedOnAdd()
+                        .HasDefaultValueSql("0");
+
+                    b.HasKey("ProductId");
+
+                    b.HasIndex("CategoryId");
+
+                    b.HasIndex("SupplierId");
+
+                    b.ToTable("Products");
+                });
+
+            modelBuilder.Entity("NorthwindTraders.Domain.Region", b =>
+                {
+                    b.Property<int>("RegionId")
+                        .HasColumnName("RegionID");
+
+                    b.Property<string>("RegionDescription")
+                        .IsRequired()
+                        .HasColumnType("nchar(50)");
+
+                    b.HasKey("RegionId");
+
+                    b.ToTable("Regions");
+                });
+
+            modelBuilder.Entity("NorthwindTraders.Domain.Shipper", b =>
+                {
+                    b.Property<int>("ShipperId")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnName("ShipperID");
+
+                    b.Property<string>("CompanyName")
+                        .IsRequired()
+                        .HasMaxLength(40);
+
+                    b.Property<string>("Phone")
+                        .HasMaxLength(24);
+
+                    b.HasKey("ShipperId");
+
+                    b.ToTable("Shippers");
+                });
+
+            modelBuilder.Entity("NorthwindTraders.Domain.Supplier", b =>
+                {
+                    b.Property<int>("SupplierId")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnName("SupplierID");
+
+                    b.Property<string>("Address")
+                        .HasMaxLength(60);
+
+                    b.Property<string>("City")
+                        .HasMaxLength(15);
+
+                    b.Property<string>("CompanyName")
+                        .IsRequired()
+                        .HasMaxLength(40);
+
+                    b.Property<string>("ContactName")
+                        .HasMaxLength(30);
+
+                    b.Property<string>("ContactTitle")
+                        .HasMaxLength(30);
+
+                    b.Property<string>("Country")
+                        .HasMaxLength(15);
+
+                    b.Property<string>("Fax")
+                        .HasMaxLength(24);
+
+                    b.Property<string>("HomePage")
+                        .HasColumnType("ntext");
+
+                    b.Property<string>("Phone")
+                        .HasMaxLength(24);
+
+                    b.Property<string>("PostalCode")
+                        .HasMaxLength(10);
+
+                    b.Property<string>("Region")
+                        .HasMaxLength(15);
+
+                    b.HasKey("SupplierId");
+
+                    b.ToTable("Suppliers");
+                });
+
+            modelBuilder.Entity("NorthwindTraders.Domain.Territory", b =>
+                {
+                    b.Property<string>("TerritoryId")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnName("TerritoryID")
+                        .HasMaxLength(20);
+
+                    b.Property<int>("RegionId")
+                        .HasColumnName("RegionID");
+
+                    b.Property<string>("TerritoryDescription")
+                        .IsRequired()
+                        .HasColumnType("nchar(50)");
+
+                    b.HasKey("TerritoryId");
+
+                    b.HasIndex("RegionId");
+
+                    b.ToTable("Territories");
+                });
+
+            modelBuilder.Entity("NorthwindTraders.Domain.Employee", b =>
+                {
+                    b.HasOne("NorthwindTraders.Domain.Employee", "Manager")
+                        .WithMany("DirectReports")
+                        .HasForeignKey("ReportsTo");
+                });
+
+            modelBuilder.Entity("NorthwindTraders.Domain.EmployeeTerritory", b =>
+                {
+                    b.HasOne("NorthwindTraders.Domain.Employee", "Employee")
+                        .WithMany("EmployeeTerritories")
+                        .HasForeignKey("EmployeeId")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.HasOne("NorthwindTraders.Domain.Territory", "Territory")
+                        .WithMany("EmployeeTerritories")
+                        .HasForeignKey("TerritoryId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+
+            modelBuilder.Entity("NorthwindTraders.Domain.Order", b =>
+                {
+                    b.HasOne("NorthwindTraders.Domain.Customer", "Customer")
+                        .WithMany("Orders")
+                        .HasForeignKey("CustomerId");
+
+                    b.HasOne("NorthwindTraders.Domain.Employee", "Employee")
+                        .WithMany("Orders")
+                        .HasForeignKey("EmployeeId");
+
+                    b.HasOne("NorthwindTraders.Domain.Shipper", "Shipper")
+                        .WithMany("Orders")
+                        .HasForeignKey("ShipVia");
+                });
+
+            modelBuilder.Entity("NorthwindTraders.Domain.OrderDetail", b =>
+                {
+                    b.HasOne("NorthwindTraders.Domain.Order", "Order")
+                        .WithMany("OrderDetails")
+                        .HasForeignKey("OrderId")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.HasOne("NorthwindTraders.Domain.Product", "Product")
+                        .WithMany("OrderDetails")
+                        .HasForeignKey("ProductId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+
+            modelBuilder.Entity("NorthwindTraders.Domain.Product", b =>
+                {
+                    b.HasOne("NorthwindTraders.Domain.Category", "Category")
+                        .WithMany("Products")
+                        .HasForeignKey("CategoryId");
+
+                    b.HasOne("NorthwindTraders.Domain.Supplier", "Supplier")
+                        .WithMany("Products")
+                        .HasForeignKey("SupplierId");
+                });
+
+            modelBuilder.Entity("NorthwindTraders.Domain.Territory", b =>
+                {
+                    b.HasOne("NorthwindTraders.Domain.Region", "Region")
+                        .WithMany("Territories")
+                        .HasForeignKey("RegionId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
