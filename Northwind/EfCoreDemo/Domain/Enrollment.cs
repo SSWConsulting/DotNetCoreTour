f747faf38a6bcb2fbd649fa1b42c54793518f649
Brendan Richards <brendan_richards_msdn@hotmail.com>
2017-07-17 16:15:05 +1000

    added domain for efcoredemo

---
+++ Northwind/EfCoreDemo/Domain/Enrollment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EfCoreDemo.Domain
+{
+    public class Enrollment
+    {
+        public int Id { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+        public int StudentId { get; set; }
+
+        public Student Student { get; set; }
+
+        public int CourseId { get; set; }
+
+        public Course Course { get; set; }
+    }
+}
---
+++ Northwind/EfCoreDemo/Domain/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EfCoreDemo.Domain
+{
+    public class Student
+    {
+
+        public int Id { get; set; }
+
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
