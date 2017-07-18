06bfd43f0986c88b90232235288fabc95ba2dbc5
Brendan Richards <brendan_richards_msdn@hotmail.com>
2017-07-18 11:39:30 +1000

    autofac assembly scanning

--- Northwind/NorthwindApi/Infrastructure/AutofacModule.cs
+++ Northwind/NorthwindApi/Infrastructure/AutofacModule.cs
@@ -1,21 +1,24 @@
-﻿using Autofac;
+﻿using System.Reflection;
+using Autofac;
 using NorthwindTraders.Application.Customers.Commands.CreateCustomer;
 using NorthwindTraders.Application.Customers.Commands.DeleteCustomer;
 using NorthwindTraders.Application.Customers.Commands.UpdateCustomer;
 using NorthwindTraders.Application.Customers.Queries.GetCustomerDetail;
 using NorthwindTraders.Application.Customers.Queries.GetCustomersList;
+using Module = Autofac.Module;
 
 namespace NorthwindTraders.NorthwindApi.Infrastructure
 {
     public class AutofacModule : Module
     {
+        
+
         protected override void Load(ContainerBuilder builder)
         {
-            builder.RegisterType<GetCustomersListQuery>().As<IGetCustomersListQuery>();
-            builder.RegisterType<GetCustomerDetailQuery>().As<IGetCustomerDetailQuery>();
-            builder.RegisterType<CreateCustomerCommand>().As<ICreateCustomerCommand>();
-            builder.RegisterType<UpdateCustomerCommand>().As<IUpdateCustomerCommand>();
-            builder.RegisterType<DeleteCustomerCommand>().As<IDeleteCustomerCommand>();
+            builder.RegisterAssemblyTypes((typeof(GetCustomersListQuery)).GetTypeInfo().Assembly)
+                .Where(t => t.Name.EndsWith("Command") || t.Name.EndsWith("Query"))
+                .AsImplementedInterfaces();
         }
+
     }
 }
